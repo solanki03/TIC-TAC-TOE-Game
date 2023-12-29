367f53c2a2615821aa5f580de1b0cb2dae6a09bf
Solanki Singha <solankisingha23@gmail.com>
2023-12-29 22:27:30 +0530

    PLayer name input section is modified. A  new method added to check player's name is left blank or not. An exception in gaming mode selction is handled.

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -6,23 +6,30 @@ public class GameLauncher {
 
         System.out.println("\nWelcome to the TIC-TAC-TOE game!\n");
         System.out.println("Choose a gaming mode:\n1. User vs User \n2. User vs Computer\n");
-        System.out.print("Enter your choice: ");
-        int num = sc.nextInt();
-
-        Players p1 = null, p2 = null;
-        Players currentPlayer;
 
         try {
+            System.out.print("Enter your choice: ");
+            int num = sc.nextInt();
+            sc.nextLine(); // it consumes the newline character
+
+            Players p1 = null, p2 = null;
+            Players currentPlayer;
+
             switch (num) {
                 case 1:
                     // User vs User
                     System.out.print("Enter name for Player 1: ");
-                    String name1 = sc.next();
+                    String name1 = sc.nextLine();
+                    name1 = checkEmptyName(name1, sc); // check the name field is given empty or not
+
                     char mark1 = selectPlayerMark(sc, name1);
                     p1 = new HumanPlayer(name1, mark1);
 
-                    System.out.print("Enter name for Player 2: ");
-                    String name2 = sc.next();
+                    sc.nextLine(); // it consumes the newline character
+                    System.out.print("\nEnter name for Player 2: ");
+                    String name2 = sc.nextLine();
+                    name2 = checkEmptyName(name2, sc); // check the name field is given empty or not
+
                     // Automatically assign the other mark to the second player
                     char mark2;
                     if (mark1 == 'X') {
@@ -36,7 +43,8 @@ public class GameLauncher {
                 case 2:
                     // User vs Computer
                     System.out.print("Enter your name: ");
-                    String userName = sc.next();
+                    String userName = sc.nextLine();
+                    userName = checkEmptyName(userName, sc); // check the name field is given empty or not
                     char userMark = selectPlayerMark(sc, userName);
                     p1 = new HumanPlayer(userName, userMark);
 
@@ -51,40 +59,52 @@ public class GameLauncher {
                     break;
 
                 default:
-                    System.out.println("Invalid input! Try again.");
+                    System.out.println("Invalid input!");
 
             }
-        } catch (Exception e) {
-        }
 
-        if (p1 != null && p2 != null) {
-            currentPlayer = p1;
-            TicTacToe.initGBoard(); // initializing the game board before starting the game
+            if (p1 != null && p2 != null) {
+                currentPlayer = p1;
+                TicTacToe.initGBoard(); // initializing the game board before starting the game
 
-            while (true) {
-                System.out.println("\n" + currentPlayer.name + "'s turn");
-                currentPlayer.makeMove(); // player make a move
-                TicTacToe.displayGBoard(); // display the game board
+                while (true) {
+                    System.out.println("\n" + currentPlayer.name + "'s turn");
+                    currentPlayer.makeMove(); // player make a move
+                    TicTacToe.displayGBoard(); // display the game board
 
-                // check the winning conditions ---
-                if (TicTacToe.checkColWin() || TicTacToe.checkRowWin() || TicTacToe.checkDiagWin()) {
-                    System.out.println(currentPlayer.name + " won the game!");
-                    break;
-                } else if (TicTacToe.checkDraw()) {
-                    System.out.println("This match is a draw!");
-                    break;
-                } else {
-                    if (currentPlayer == p1) {
-                        currentPlayer = p2;
+                    // check the winning conditions ---
+                    if (TicTacToe.checkColWin() || TicTacToe.checkRowWin() || TicTacToe.checkDiagWin()) {
+                        System.out.println(currentPlayer.name + " won the game!");
+                        break;
+                    } else if (TicTacToe.checkDraw()) {
+                        System.out.println("This match is a draw!");
+                        break;
                     } else {
-                        currentPlayer = p1;
+                        if (currentPlayer == p1) {
+                            currentPlayer = p2;
+                        } else {
+                            currentPlayer = p1;
+                        }
                     }
                 }
+            } else {
+                System.out.println("Error occured while initializing players. Exiting the game!");
             }
-        } else {
-            System.out.println("Error occured while initializing players. Exiting the game!");
+
+        } catch (Exception e) {
+            System.out.println("Invalid Input has given. Exiting the game!...");
         }
 
+    }
+
+    // Check the name field is empty or not
+    private static String checkEmptyName(String name, Scanner sc) {
+        if (name.isBlank()) {
+            System.out.print("This field is empty!\nPlease enter your name: ");
+            name = sc.nextLine();
+            return name;
+        }
+        return name;
     }
 
     // method to select player's mark
@@ -94,19 +114,25 @@ public class GameLauncher {
 
         char mark;
         while (true) {
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    mark = 'X';
-                    break;
-                case 2:
-                    mark = 'O';
-                    break;
-                default:
-                    System.out.print("Invalid choice! Choose 1 for 'X' or 2 for 'O': ");
-                    continue; // Go back to the beginning of the loop
+            try {
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        mark = 'X';
+                        break;
+                    case 2:
+                        mark = 'O';
+                        break;
+                    default:
+                        System.out.print("Invalid choice! Choose 1 for 'X' or 2 for 'O': ");
+                        continue; // Go back to the beginning of the loop
+                }
+                break; // Exit the loop if a valid choice is made
+            } catch (Exception ex) {
+                System.out.println("Invalid input in exception! Please enter a number: ");
+                sc.nextInt();
+                // sc.nextLine(); // suggested by gpt
             }
-            break; // Exit the loop if a valid choice is made
         }
 
         return mark;
